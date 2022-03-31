49a991c44d952288cbbbfd11dcec5ed5a6bc82ce
filesdev7 <filesdev7@gmail.com>
2022-03-31 17:54:12 +0200

    emad

--- app/src/main/java/com/apps/dbrah/model/SignUpModel.java
+++ app/src/main/java/com/apps/dbrah/model/SignUpModel.java
@@ -1,6 +1,7 @@
 package com.apps.dbrah.model;
 
 import android.content.Context;
+import android.util.Patterns;
 
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
@@ -11,78 +12,100 @@ import com.apps.dbrah.R;
 
 
 public class SignUpModel extends BaseObservable {
-    private String first_name;
-    private String seconed_name;
+    private String image_uri;
+    private String phone_code;
+    private String phone;
+    private String name;
+    private String email;
+    private String vat;
+    private boolean valid;
 
-
-    public ObservableField<String> error_first_name = new ObservableField<>();
-    public ObservableField<String> error_seconed_name = new ObservableField<>();
-
-
-    public boolean isDataValid(Context context) {
-        if (!first_name.trim().isEmpty()
-                &&
-                !seconed_name.trim().isEmpty() //&&
-//               department_id != 0
-
+    public void isDataValid() {
+        if (!name.trim().isEmpty() &&
+                Patterns.EMAIL_ADDRESS.matcher(email.trim()).matches() &&
+                !vat.isEmpty()
 
         ) {
-            error_first_name.set(null);
-            error_seconed_name.set(null);
-
-            return true;
         } else {
 
-            if (first_name.trim().isEmpty()) {
-                error_first_name.set(context.getString(R.string.field_required));
-
-            } else {
-                error_first_name.set(null);
-
-            }
-            if (seconed_name.trim().isEmpty()) {
-                error_seconed_name.set(context.getString(R.string.field_required));
-
-            } else {
-                error_seconed_name.set(null);
-
-            }
 
 
-            return false;
         }
     }
 
     public SignUpModel() {
-
-        setFirst_name("");
-
-        setSeconed_name("");
+        phone_code = "";
+        phone="";
+        name ="";
+        email ="";
+        vat ="";
 
     }
 
+    public String getImage_uri() {
+        return image_uri;
+    }
+
+    public void setImage_uri(String image_uri) {
+        this.image_uri = image_uri;
+    }
+
+    public String getPhone_code() {
+        return phone_code;
+    }
+
+    public void setPhone_code(String phone_code) {
+        this.phone_code = phone_code;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
     @Bindable
-    public String getFirst_name() {
-        return first_name;
+    public String getName() {
+        return name;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-        notifyPropertyChanged(BR.first_name);
-
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+        isDataValid();
     }
 
     @Bindable
-    public String getSeconed_name() {
-        return seconed_name;
+    public String getEmail() {
+        return email;
     }
 
-    public void setSeconed_name(String seconed_name) {
-        this.seconed_name = seconed_name;
-        notifyPropertyChanged(BR.seconed_name);
-
+    public void setEmail(String email) {
+        this.email = email;
+        notifyPropertyChanged(BR.email);
+        isDataValid();
     }
 
+    @Bindable
+    public String getVat() {
+        return vat;
+    }
 
+    public void setVat(String vat) {
+        this.vat = vat;
+        notifyPropertyChanged(BR.vat);
+        isDataValid();
+    }
+
+    @Bindable
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+        notifyPropertyChanged(BR.valid);
+    }
 }
--- app/src/main/java/com/apps/dbrah/uis/activity_sign_up/SignUpActivity.java
+++ app/src/main/java/com/apps/dbrah/uis/activity_sign_up/SignUpActivity.java
@@ -36,12 +36,8 @@ public class SignUpActivity extends BaseActivity {
     private Preferences preferences;
     private String phone_code, phone;
     private ActivityResultLauncher<Intent> launcher;
-    private final String READ_PERM = Manifest.permission.READ_EXTERNAL_STORAGE;
-    private final String write_permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
-    private final String camera_permission = Manifest.permission.CAMERA;
-    private final int READ_REQ = 1, CAMERA_REQ = 2;
-    private int selectedReq = 0;
     private Uri uri = null;
+    private int req;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -55,148 +51,81 @@ public class SignUpActivity extends BaseActivity {
         Intent intent = getIntent();
         phone_code = intent.getStringExtra("phone_code");
         phone = intent.getStringExtra("phone");
+
     }
 
     private void initView() {
         preferences = Preferences.getInstance();
         model = new SignUpModel();
+        model.setPhone_code(phone_code);
+        model.setPhone(phone);
         binding.setModel(model);
         launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-            if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                if (selectedReq == READ_REQ) {
-                    binding.icon.setVisibility(View.GONE);
+            if (result.getResultCode() == RESULT_OK) {
 
-                    uri = result.getData().getData();
-                    File file = new File(Common.getImagePath(this, uri));
-                    //Picasso.get().load(file).fit().into(binding.image);
-
-                } else if (selectedReq == CAMERA_REQ) {
-                    Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
-                    binding.icon.setVisibility(View.GONE);
-                    uri = getUriFromBitmap(bitmap);
-                    if (uri != null) {
-                        String path = Common.getImagePath(this, uri);
-
-                        if (path != null) {
-                            //Picasso.get().load(new File(path)).fit().into(binding.image);
-
-                        } else {
-                            // Picasso.get().load(uri).fit().into(binding.image);
-
-                        }
-                    }
-                }
             }
         });
 
 
-        binding.flImage.setOnClickListener(view -> openSheet());
-        binding.flGallery.setOnClickListener(view -> {
-            closeSheet();
-            checkReadPermission();
-        });
-
-        binding.flCamera.setOnClickListener(view -> {
-            closeSheet();
-            checkCameraPermission();
-        });
-
-        binding.btnCancel.setOnClickListener(view -> closeSheet());
-
+        binding.flImage.setOnClickListener(v -> checkPhotoPermission());
         binding.btnSignup.setOnClickListener(view -> {
-            if (model.isDataValid(this)) {
-                if (model.isDataValid(this)) {
 
-                }
-            }
         });
 
     }
 
-    public void openSheet() {
-        binding.expandLayout.setExpanded(true, true);
-    }
 
-    public void closeSheet() {
-        binding.expandLayout.collapse(true);
+    public void checkPhotoPermission() {
 
-    }
 
-    public void checkReadPermission() {
-        closeSheet();
-        if (ActivityCompat.checkSelfPermission(this, READ_PERM) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{READ_PERM}, READ_REQ);
-        } else {
-            SelectImage(READ_REQ);
-        }
-    }
-
-    public void checkCameraPermission() {
-
-        closeSheet();
-
-        if (ContextCompat.checkSelfPermission(this, write_permission) == PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, camera_permission) == PackageManager.PERMISSION_GRANTED
+        if (ContextCompat.checkSelfPermission(this, WRITE_PERM) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, CAM_PERM) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, READ_PERM) == PackageManager.PERMISSION_GRANTED
         ) {
-            SelectImage(CAMERA_REQ);
+            SelectImage();
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{camera_permission, write_permission}, CAMERA_REQ);
+            ActivityCompat.requestPermissions(this, new String[]{READ_PERM, WRITE_PERM, CAM_PERM}, req);
         }
     }
 
-    private void SelectImage(int req) {
-        selectedReq = req;
-        Intent intent = new Intent();
+    private void SelectImage() {
+        Intent intentGallery = new Intent();
+        intentGallery.setAction(Intent.ACTION_GET_CONTENT);
+        intentGallery.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intentGallery.setType("image/*");
 
-        if (req == READ_REQ) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
-                intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
-            } else {
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-
-            }
-
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            intent.setType("image/*");
-
-            launcher.launch(intent);
-
-        } else if (req == CAMERA_REQ) {
-            try {
-                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                launcher.launch(intent);
-            } catch (SecurityException e) {
-                Toast.makeText(this, R.string.perm_image_denied, Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
-                Toast.makeText(this, R.string.perm_image_denied, Toast.LENGTH_SHORT).show();
-
-            }
+        Intent intentCamera = new Intent();
+        intentCamera.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri outPutUri = null;
+        intentCamera.putExtra(MediaStore.EXTRA_OUTPUT,uri);
 
 
-        }
+
+
     }
+
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == READ_REQ) {
+        if (requestCode == req) {
 
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                SelectImage(requestCode);
+            if (grantResults.length > 0) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                        grantResults[1] == PackageManager.PERMISSION_GRANTED &&
+                        grantResults[2] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    SelectImage();
+
+                } else {
+                    Toast.makeText(this, getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
+
+                }
             } else {
                 Toast.makeText(this, getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
             }
 
-        } else if (requestCode == CAMERA_REQ) {
-            if (grantResults.length > 0 &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-
-                SelectImage(requestCode);
-            } else {
-                Toast.makeText(this, getString(R.string.perm_image_denied), Toast.LENGTH_SHORT).show();
-            }
         }
     }
 
